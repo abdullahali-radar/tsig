a74a331f07f789ab4a2fa75570c7d03c7eb7963f
abdullahali-radar <alibinakhsan@gmail.com>
2022-11-09 07:14:02 +0000

    Second Commit

--- run.py
+++ run.py
@@ -8,13 +8,26 @@ main = Flask(__name__)
 #run_with_ngrok(main)
 
 fileAbout='./aboutProject.json'
-f=open(fileAbout)
-about=json.load(f)
-f.close()
+fileKI='./ki_clip.geojson'
+fileRH='./rh_clip_more70.geojson'
+fileMSLP='./mslp_clip.geojson'
+fileTP='./tp_clip_more0.geojson'
+
+def openJSON(file):
+  with open(fileAbout) as f:
+    content=json.load(f)
+  return content
 
 @main.route("/")
 def helloWorld():
   return Response(response=json.dumps(about),
+                    status=200,
+                    mimetype="application/json")
+
+@main.route("/ki")
+def displayKI():
+  ki=openJSON(fileKI)
+  return Response(response=json.dumps(ki),
                     status=200,
                     mimetype="application/json")
 
