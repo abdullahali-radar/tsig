eddb5901ab5df01c200673e99c58e35adc28ddb5
abdullahali-radar <alibinakhsan@gmail.com>
2022-11-09 06:51:52 +0000

    Second Commit

--- run.py
+++ run.py
@@ -7,13 +7,13 @@ main = Flask(__name__)
 
 @main.route("/")
 def helloWorld():
-  fileAbout='/content/tsig/aboutProject.json'
+  fileAbout='tsig/aboutProject.json'
   f=open(fileAbout)
   about=json.load(f)
   f.close()
   return Response(response=json.dumps(about),
                     status=200,
                     mimetype="application/json")
-                    
+
 if __name__ == '__main__':
   main.run()
