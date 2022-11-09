80d4e1679ec0b5f5812bec4f6af25cfa5cb6fadf
abdullahali-radar <alibinakhsan@gmail.com>
2022-11-09 06:59:22 +0000

    Second Commit

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import Response
 from flask import request
+#from flask_ngrok import run_with_ngrok
 import json
 
 main = Flask(__name__)
+#run_with_ngrok(main)
 
-fileAbout='/content/tsig/aboutProject.json'
+fileAbout='/aboutProject.json'
 f=open(fileAbout)
 about=json.load(f)
 f.close()
