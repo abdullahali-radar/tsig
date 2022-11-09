5c7b521c60b8bd089bc7e1e597f321fe6f316850
abdullahali-radar <alibinakhsan@gmail.com>
2022-11-09 06:48:26 +0000

    Second Commit

--- run.py
+++ run.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from flask import Response
+from flask import request
+import json
 
 main = Flask(__name__)
 
 @main.route("/")
 def helloWorld():
-  return 'Hello World Flask!'
-
+  fileAbout='/content/tsig/aboutProject.json'
+  f=open(fileAbout)
+  about=json.load(f)
+  f.close()
+  return Response(response=json.dumps(about),
+                    status=200,
+                    mimetype="application/json")
+                    
 if __name__ == '__main__':
   main.run()
