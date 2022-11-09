8be828f44f5dcd973b5b9a23e92dd40d58e223d5
abdullahali-radar <alibinakhsan@gmail.com>
2022-11-09 09:45:49 +0000

    Third Commit

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask import Response
 from flask import request
-from flask_ngrok import run_with_ngrok
+#from flask_ngrok import run_with_ngrok
 import json
 
 main = Flask(__name__)
-run_with_ngrok(main)
+#run_with_ngrok(main)
 
 fileAbout='./aboutProject.json'
 fileKI='./ki_jateng.geojson'
